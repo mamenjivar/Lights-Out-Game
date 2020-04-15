ba5557818bb14786ff872b86a07c028c2986da46
Miguel Menjivar <menjivarmiguel221@gmail.com>
2020-04-14 20:24:10 -0700

    managed to add new features to project, needs to add toggle feature to top/bottom left/right of selected tile to finish concept of game

--- GameEngine.java
+++ GameEngine.java
@@ -13,7 +13,12 @@ public class GameEngine {
     UserInterface ui;
     Scanner kb;
 
+    // stores value of main menu option
+    // chosen by user
     int mainMenuChoice;
+
+    // test board with fixed length, width
+    boolean[][] testBoardGame;
 
     /**
      * Default constructor
@@ -21,6 +26,7 @@ public class GameEngine {
     GameEngine() {
         ui = new UserInterface();
         kb = new Scanner(System.in);
+        testBoardGame = new boolean[5][5];
     }
 
     /**
@@ -45,6 +51,7 @@ public class GameEngine {
 
             try{
                 mainMenuChoice = kb.nextInt();
+                kb.nextLine();
                 
             } catch(Exception e){
                 System.out.println("");
@@ -57,6 +64,7 @@ public class GameEngine {
             switch (mainMenuChoice) {
                 case 1: // play game
                     mainMenuLoop = false;
+                    playGame();
                     break;
                 case 2: // how-to-play-game
                     mainMenuLoop = false;
@@ -71,7 +79,50 @@ public class GameEngine {
         }
     }
 
+    /**
+     * play game 
+     */
+    public void playGame() {
+        ui.testBoardBlank();
+        ui.testBoardFilled();
 
+        populateTestBoardFixed();
+
+        ui.booleanTestBoard(testBoardGame);
+
+        // test tile 
+        toggleBoard();
+
+        ui.booleanTestBoard(testBoardGame);
+
+
+    }
+
+    /**
+     * will toggle tile on or off
+     */
+    public void toggleBoard() {
+        String toggleTile;
+
+        try{
+            toggleTile = kb.nextLine();
+            String[] split = toggleTile.split("");
+
+            int row = Integer.parseInt(split[0]);
+            int col = Integer.parseInt(split[1]);
+
+            testBoardGame[row][col] = false;
+        } catch(Exception e) {
+            System.out.println("");
+            System.out.println(e);
+            kb.next();
+            ui.notANumberError();
+        }
+    }
+
+    public boolean didYouWin() {
+        return true;
+    }
 
     /**
      * When the user decides
@@ -79,6 +130,48 @@ public class GameEngine {
      */
     public void exit(){
         ui.exitGame();
+        kb.close();
         System.exit(0);
+    }
+
+    /**
+     * Test board with fixed 
+     * values to test game
+     */
+    public void populateTestBoardFixed() {
+        // 1st row
+        testBoardGame[0][0] = false;
+        testBoardGame[0][1] = false;
+        testBoardGame[0][2] = true;
+        testBoardGame[0][3] = false;
+        testBoardGame[0][4] = false;
+
+        // 2nd row
+        testBoardGame[1][0] = false;
+        testBoardGame[1][1] = true;
+        testBoardGame[1][2] = false;
+        testBoardGame[1][3] = false;
+        testBoardGame[1][4] = false;
+
+        // 3rd row
+        testBoardGame[2][0] = false;
+        testBoardGame[2][1] = true;
+        testBoardGame[2][2] = false;
+        testBoardGame[2][3] = false;
+        testBoardGame[2][4] = false;
+
+        // 4th row
+        testBoardGame[3][0] = false;
+        testBoardGame[3][1] = false;
+        testBoardGame[3][2] = false;
+        testBoardGame[3][3] = true;
+        testBoardGame[3][4] = false;
+
+        // 5th row
+        testBoardGame[4][0] = false;
+        testBoardGame[4][1] = false;
+        testBoardGame[4][2] = false;
+        testBoardGame[4][3] = true;
+        testBoardGame[4][4] = false;
     }
 }
--- UserInterface.java
+++ UserInterface.java
@@ -10,6 +10,7 @@
 public class UserInterface {
     public void title() {
         System.out.println("Light's Out!!");
+        System.out.println("");
     }
 
     /**
@@ -46,5 +47,88 @@ public class UserInterface {
      */
     public void exitGame(){
         System.out.println("Game Exited...\n Goodbye...");
+    }
+
+
+    public void testBoardBlank() {
+        System.out.println("_____________________");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|___|___|___|___|___|");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|___|___|___|___|___|");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|___|___|___|___|___|");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|___|___|___|___|___|");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|   |   |   |   |   |");
+        System.out.println("|___|___|___|___|___|");
+    }
+
+    public void testBoardFilled() {
+        System.out.println("__________________________");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| 00 | 01 | 02 | 03 | 04 |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| 10 | 11 | 12 | 13 | 14 |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| 20 | 21 | 22 | 23 | 24 |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| 30 | 31 | 32 | 33 | 34 |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| 40 | 41 | 42 | 43 | 44 |");
+        System.out.println("|____|____|____|____|____|");
+    }
+
+    public void booleanTestBoard(boolean[][] testBoardPrint) {
+        
+        System.out.println("__________________________");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| " + ((testBoardPrint[0][0] == true) ? "00" : "  ") + " | " + ((testBoardPrint[0][1] == true) ? "01" : "  ")  + " | " + ((testBoardPrint[0][2] == true) ? "02" : "  ") + " | " + ((testBoardPrint[0][3] == true) ? "03" : "  ") + " | " + ((testBoardPrint[0][4] == true) ? "04" : "  ") +" |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| " + ((testBoardPrint[1][0] == true) ? "10" : "  ") + " | "
+                + ((testBoardPrint[1][1] == true) ? "11" : "  ") + " | "
+                + ((testBoardPrint[1][2] == true) ? "12" : "  ") + " | "
+                + ((testBoardPrint[1][3] == true) ? "13" : "  ") + " | "
+                + ((testBoardPrint[1][4] == true) ? "14" : "  ") + " |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| " + ((testBoardPrint[2][0] == true) ? "20" : "  ") + " | "
+                + ((testBoardPrint[2][1] == true) ? "21" : "  ") + " | "
+                + ((testBoardPrint[2][2] == true) ? "22" : "  ") + " | "
+                + ((testBoardPrint[2][3] == true) ? "23" : "  ") + " | "
+                + ((testBoardPrint[2][4] == true) ? "24" : "  ") + " |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| " + ((testBoardPrint[3][0] == true) ? "30" : "  ") + " | "
+                + ((testBoardPrint[3][1] == true) ? "31" : "  ") + " | "
+                + ((testBoardPrint[3][2] == true) ? "32" : "  ") + " | "
+                + ((testBoardPrint[3][3] == true) ? "33" : "  ") + " | "
+                + ((testBoardPrint[3][4] == true) ? "34" : "  ") + " |");
+        System.out.println("|____|____|____|____|____|");
+        System.out.println("|    |    |    |    |    |");
+        System.out.println("| " + ((testBoardPrint[4][0] == true) ? "40" : "  ") + " | "
+                + ((testBoardPrint[4][1] == true) ? "41" : "  ") + " | "
+                + ((testBoardPrint[4][2] == true) ? "42" : "  ") + " | "
+                + ((testBoardPrint[4][3] == true) ? "43" : "  ") + " | "
+                + ((testBoardPrint[4][4] == true) ? "44" : "  ") + " |");
+        System.out.println("|____|____|____|____|____|");
+    }
+
+    public void testBoardArray(boolean[][] array) {
+        for(int row = 0; row < array.length; row++){
+            for(int col = 0; col < array[row].length; col++){
+                System.out.println(array[row][col]);
+            }
+        }
     }
 }
